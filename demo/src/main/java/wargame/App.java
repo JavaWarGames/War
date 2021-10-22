30bf5ef5cd21936fd1c4644c1cb7fee68cf37dc8
Ronald Rajski <ronaldrajski@gmail.com>
2021-10-21 19:49:23 -0500

    attempt 2

---
+++ demo/src/main/java/wargame/App.java
@@ -0,0 +1,14 @@
+package demo.src.main.java.wargame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
