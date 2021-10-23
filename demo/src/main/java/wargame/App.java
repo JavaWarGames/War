84a7c64768fbac265cf7e063f52793d00f677eaf
Ronald Rajski <ronaldrajski@gmail.com>
2021-10-23 14:18:40 -0500

    using programming assignment 2 for framework

--- demo/src/main/java/wargame/App.java
+++ demo/src/main/java/wargame/App.java
@@ -1,12 +1,23 @@
 package demo.src.main.java.wargame;
 
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+    
+};
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App extends Application {
+    public void start(Stage primaryStage){
+
+        Image imageX = new ImageX
+    }
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
