5032e079729f0fb5e0112b79bad2a8830d54dbdb
Ronald Rajski <ronaldrajski@gmail.com>
2021-10-23 19:30:58 -0500

    plugged code in, but it does not use JAVA FX specificaly

--- demo/src/main/java/wargame/App.java
+++ demo/src/main/java/wargame/App.java
@@ -1,27 +1,36 @@
 package demo.src.main.java.wargame;
 
-import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.Image;
-    
-};
-/**
- * Hello world!
- *
- */
-public class App extends Application {
-    public void start(Stage primaryStage){
+// import javafx.application.Application;
+// import javafx.geometry.Pos;
+// import javafx.scene.Scene;
+// import javafx.scene.layout.GridPane;
+// import javafx.stage.Stage;
+// import javafx.scene.image.Image;
+ 
 
-        Image imageX = new ImageX
-    }
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+
+    
+
+// public class App extends Application {
+//     public void start(Stage primaryStage){
+
+//         Image imageX = new Image("image/x.gif");
+//         Image ImageO = new Image("image/o.gif");
+
+//         GridPane pane =  new GridPane();
+//         pane.setAlignment(Pos.CENTER);
+//         pane.setHgap(5);
+//         pane.setVgap(5);
+
+//         for(int i = 0; i < 3; i++){
+//             for(int j = 0; j<3; j++){
+
+//             }
+//         }
+
+//     }
+public class Main {
+    public static void main(String[] args) {
+        TicTacToe ticTacToe = new TicTacToe();
     }
 }
